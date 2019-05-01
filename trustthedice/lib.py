0aa1c438e21508b7d539484e7f37f511476d4dea
Sujay Mansingh <sujay.mansingh@gmail.com>
2019-05-01 22:27:13 +0100

    Add func to actually pick an outcome

--- trustthedice/lib.py
+++ trustthedice/lib.py
@@ -50,6 +50,32 @@ def calculate_cumulative_probabilities(outcomes, remainder_name=None):
     return result
 
 
+def pick_outcome(value, outcomes):
+    """Pick the first outcome that has a probability that is greater than the given value.
+
+    >>> o1 = ProbableOutcome(name="a", probability=Fraction(1, 3))
+    >>> o2 = ProbableOutcome(name="b", probability=Fraction(2, 3))
+    >>> o3 = ProbableOutcome(name="c", probability=Fraction(3, 3))
+    >>> outcomes = [o1, o2, o3]
+
+    Anything less than (or equal to) 1/3 will give us a...
+    >>> pick_outcome(0.001, outcomes).name
+    'a'
+    >>> pick_outcome(0.333, outcomes).name
+    'a'
+    >>> pick_outcome(Fraction(1, 3), outcomes).name
+    'a'
+
+    Between 1/3 and 2/3 gives us 'b'
+    >>> pick_outcome(0.4, outcomes).name
+    'b'
+    """
+    for outcome in outcomes:
+        if value <= outcome.probability:
+            return outcome
+    raise CouldntPickOutcomeError()
+
+
 class BaseError(Exception):
     pass
 
@@ -59,6 +85,10 @@ class TotalProbabilityMoreThanOneError(BaseError):
 
 
 class TotalProbabilityLessThanOneError(BaseError):
+    pass
+
+
+class CouldntPickOutcomeError(BaseError):
     pass
 
 
