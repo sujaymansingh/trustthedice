ceb81c33b4c160709b20fe520a77713ed853eb7d
Sujay Mansingh <sujay.mansingh@gmail.com>
2019-05-01 22:49:18 +0100

    Allow cli to pick a random outcome

--- trustthedice/cli.py
+++ trustthedice/cli.py
@@ -1,4 +1,27 @@
+import random
+
 import click
+
+from . import lib
+
+
+class ProbableOutcomeParamType(click.ParamType):
+    name = "ProbableOutcome"
+
+    def convert(self, value, _param, _ctx):
+        return lib.parse_probable_outcome(value)
+
+
+@contextmanager
+def catch_errors():
+    try:
+        yield
+    except lib.BaseError as e:
+        click.echo("Encountered an error:")
+        click.echo(dedent(e.explanation()))
+    except:
+        click.echo("unknown error")
+        raise
 
 
 @click.group()
@@ -6,6 +29,16 @@ def main():
     pass
 
 
-@main.command()
-def generate():
-    click.echo("TODO")
+@main.command("random")
+@click.option(
+    "--outcome", "-oc", "outcomes", multiple=True, type=ProbableOutcomeParamType()
+)
+@click.option("--otherwise", type=str, default="n/a")
+def pick_random_outcome(outcomes, otherwise):
+    outcomes = lib.calculate_cumulative_probabilities(
+        outcomes, remainder_name=otherwise
+    )
+    value = random.random()
+    chosen_outcome = lib.pick_outcome(value, outcomes)
+
+    click.echo(chosen_outcome.name)
