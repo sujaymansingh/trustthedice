8844a5f88f451c22b3eac8467122ef000b7e6d3b
Sujay Mansingh <sujay.mansingh@gmail.com>
2019-05-06 21:05:29 +0100

    Fixup setup.py for welease

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 from setuptools import setup
 
+
 setup(
     name="trustthedice",
     version="0.0.1",
     py_modules=["trustthedice"],
-    install_requires=[
-        "attrs==19.1.0",
-        "black==19.3b0",
-        "Click==7.0",
-        "pytest==4.4.1"
-    ],
+    author="Sujay Mansingh",
+    author_email="info@sujaymansingh.com",
+    description="Let random numbers rule your life!",
+    url="https://github.com/sujaymansingh/trustthedice",
+    install_requires=["attrs==19.1.0", "black==19.3b0", "Click==7.0", "pytest==4.4.1"],
     entry_points="""
         [console_scripts]
         trustthedice=trustthedice.cli:main
     """,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
